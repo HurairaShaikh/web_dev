412fec0023081cee87d3733df73ab28ddf7daa7b
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-04-04 23:10:05 +0500

    bootstrap

---
+++ script.js
@@ -0,0 +1,8 @@
+// let button=document.querySelector("button");
+
+// button.addEventListener("click",alertmsg);
+
+// function alertmsg(){
+// let name =prompt("Enter name");
+// button.textContent ="Your name "+name;
+// }
