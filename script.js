7d741dec931587deec6b6b99abb2e6bf4e24111a
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-04-04 23:10:29 +0500

    js added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// let button=document.querySelector("button");
+let button=document.querySelector("button");
 
-// button.addEventListener("click",alertmsg);
+button.addEventListener("click",alertmsg);
 
-// function alertmsg(){
-// let name =prompt("Enter name");
-// button.textContent ="Your name "+name;
-// }
+function alertmsg(){
+let name =prompt("Enter name");
+button.textContent ="Your name "+name;
+}
