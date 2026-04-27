2200e8a01b55ced21c2cc8cbd77e4eacf8aee5a3
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-04-27 23:25:53 +0500

    Practices

--- script.js
+++
@@ -1,8 +0,0 @@
-let button=document.querySelector("button");
-
-button.addEventListener("click",alertmsg);
-
-function alertmsg(){
-let name =prompt("Enter name");
-button.textContent ="Your name "+name;
-}
