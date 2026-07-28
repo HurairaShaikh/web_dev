8c278215fa7283dddf6877ceaa53077ec282470d
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-07-29 01:08:55 +0500

    Rock Paper Scissors

--- Rock_Paper_Scissors/app.js
+++ Rock_Paper_Scissors/app.js
@@ -11,17 +11,46 @@ choices.forEach((choice) => {
     });
 });
 
-const playGame=(userchoice)=>{
-    console.log("user choice = ",userchoice);
-    const compchoice=computerChoice();
-    console.log("Comp Choice = ",compchoice);
+const playGame = (userchoice) => {
+    console.log("user choice = ", userchoice);
+    const compchoice = computerChoice();
+    console.log("Comp Choice = ", compchoice);
 
+    if (userchoice === compchoice) {
+        drawGame();
+    }
+    else {
+        let userwin = true;
+        if (userchoice === "rock") {
+            userwin = compchoice === "scissors" ? true : false;
+        }
+        else if (userchoice === "paper") {
+            userwin = compchoice === "rock" ? true : false;
+        }
+        else {
+            userwin = compchoice === "rock" ? true : false;
+        }
+        showWinner(user);
+    }
 
 }
 
+const showWinner = (userwin) => {
+    if (userwin) {
+        console.log("You win");
+    }
+    else {
+        console.log("Computer win");
+    }
+}
 
-const computerChoice=()=>{
-    const options=["rock","paper","scissors"];
-    const randomidx=Math.floor(Math.random() *3);
+
+const computerChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randomidx = Math.floor(Math.random() * 3);
     return options[randomidx];
+}
+
+const drawGame = () => {
+    console.log("Game was Drawed");
 }
