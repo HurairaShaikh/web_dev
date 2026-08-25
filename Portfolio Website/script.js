0049fd515b480906be2594722c6249bb85797c4f
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-08-25 23:33:02 +0500

    Portfolio

--- Portfolio Website/script.js
+++ Portfolio Website/script.js
@@ -1,7 +1,7 @@
 let element = document.getElementById("element");
 
-  var typed = new Typed(element, {
-            strings: ['Software Engineer','FrontEnd Developer','Web Developer', 'Mobile App Developer'],
-            typeSpeed: 50,
-            
-        });
+var typed = new Typed(element, {
+    strings: ['Software Engineer', 'FrontEnd Developer', 'Web Developer', 'Mobile App Developer'],
+    typeSpeed: 50,
+    loop: true
+});
