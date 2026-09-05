ead3255d80b41bf6e6fade554b7b1aac11c564cb
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-09-06 01:43:31 +0500

    Whole Portfolio

--- Portfolio Webiste/script.js
+++ Portfolio Webiste/script.js
@@ -5,3 +5,32 @@ var typed = new Typed(element, {
     typeSpeed: 50,
     loop: true
 });
+
+const form = document.getElementById("contact-form");
+const successMessage = document.getElementById("toast");
+
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const response = await fetch(form.action, {
+        method: "POST",
+        body: new FormData(form),
+        headers: {
+            "Accept": "application/json"
+        }
+    });
+
+    if (response.ok) {
+        form.reset();
+        toast.textContent = "✓ Message sent successfully!";
+        toast.classList.add("show");
+
+        setTimeout(() => {
+            toast.classList.remove("show");
+        }, 3000);
+
+    } else {
+        successMessage.textContent =
+            "Something went wrong. Please try again.";
+    }
+});
