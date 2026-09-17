c650d3a4186fa8d3c3f593a91321139f45a3993f
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-09-17 23:49:38 +0500

    Renaming Document

---
+++ Portfolio Webiste/script.js
@@ -0,0 +1,46 @@
+let element = document.getElementById("element");
+
+var typed = new Typed(element, {
+    strings: ['Software Engineer', 'FrontEnd Developer', 'Web Developer', 'Mobile App Developer'],
+    typeSpeed: 50,
+    loop: true
+});
+
+const form = document.getElementById("contact-form");
+const successMessage = document.getElementById("toast");
+
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const response = await fetch(form.action, {
+        method: "POST",
+        body: new FormData(form),
+        headers: {
+            "Accept": "application/json"
+        }
+    });
+
+    if (response.ok) {
+        form.reset();
+        toast.textContent = "✓ Message sent successfully!";
+        toast.classList.add("show");
+
+        setTimeout(() => {
+            toast.classList.remove("show");
+        }, 3000);
+
+    } else {
+        successMessage.textContent =
+            "Something went wrong. Please try again.";
+    }
+});
+
+
+const menuBtn = document.getElementById("menu-btn");
+const dropdown = document.getElementById("dropdown");
+
+menuBtn.addEventListener("click", () => {
+
+    dropdown.classList.toggle("active");
+
+});
